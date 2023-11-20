2c5be09c3a38c1a2e5fc85f08fdf385a3a365686
grgkimari <grgkimari@gmail.com>
2023-11-20 21:26:23 +0300

    Initial Commit

---
+++ WeatherAppUI/WeatherAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppUIApp.swift
+//  WeatherAppUI
+//
+//  Created by George on 20/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
